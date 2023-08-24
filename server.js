0043bda7948a882213a9964d15db22550a0ce68c
Yue Yu <yueyu@YuedeMacBook-Pro.local>
2023-08-24 15:56:55 -0500

    group songs and improve ui

--- server.js
+++ server.js
@@ -33,15 +33,61 @@ app.get('/singers', async(req, res) => {
 
 app.get('/songs/:name', async(req, res) => {
     const dir = `../../../Music/吉他谱/${req.params.name}`;
-    const songs = fs.readdirSync(dir);
-    res.json({ songs })
+    const songs = fs.readdirSync(dir).filter(f => f !== '.DS_Store');
+
+    const set = new Set();
+    const songNames = [];
+
+    for(const s of songs) {
+        const strs = s.split('.');
+        let name = '';
+        if(strs.length === 1) {
+            name = strs[0];
+        } else if (strs.length === 2) {
+            name = strs[0];
+        } else if (strs.length === 3) {
+            name = strs[1];
+        }
+
+        const lastChar = name[name.length - 1];
+        if(lastChar >= '0' && lastChar <= '9') {
+            name = name.substring(0, name.length - 1);
+        }
+
+        if(!set.has(name)) {
+            songNames.push(name);
+            set.add(name);
+        }
+    }
+
+    res.json({ songs: songNames })
 })
 
-app.get('/songs/:name/:image', async(req, res) => {
-    const imageDir = `../../../Music/吉他谱/${req.params.name}/${req.params.image}`;
+app.get('/songs/:name/:song', async(req, res) => {
+    const singerDir = `../../../Music/吉他谱/${req.params.name}`;
+    const songs = fs.readdirSync(singerDir);
+    const images = songs.filter(song => song.indexOf(req.params.song) >= 0);
+    console.log('images', images);
+    const imageSources = []
+    images.forEach(image => {
+        const imageDir = `../../../Music/吉他谱/${req.params.name}/${image}`;
+        const contents = fs.readFileSync(imageDir);
+        const b64 = contents.toString('base64')
+        const type = imageType(contents);
+        const src = `data:${type.mime};base64,${b64}`;
+        imageSources.push(src);
+    })
+
+    res.send({ imageSources })
+
+/*
+    const imageDir = `../../../Music/吉他谱/${req.params.name}/${req.params.song}`;
+
     const contents = fs.readFileSync(imageDir);
     const b64 = contents.toString('base64')
     const type = imageType(contents);
     const src = `data:${type.mime};base64,${b64}`;
+
     res.send({ src })
+*/
 })
--- src/Singers.jsx
+++ src/Singers.jsx
@@ -1,11 +1,24 @@
 import React, { useState } from "react";
+import { Button } from "@mui/material";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import './Singers.css';
+
+const theme = createTheme({
+    palette: {
+      song: {
+        main: '#cf84f0',
+        contrastText: '#47008F',
+      },
+    },
+  });
 
 const Singers = () => {
     const [names, setNames] = useState([]);
+    const [selectedSinger, setSelectedSinger] = useState(-1);
     const [singer, setSinger] = useState('');
+    const [selectedSong, setSelectedSong] = useState(-1);
     const [songs, setSongs] = useState([]);
-    const [image, SetImage] = useState('');
+    const [imageSources, SetImageSources] = useState([]);
 
     fetch('/singers')
         .then(res => res.json())
@@ -14,42 +27,78 @@ const Singers = () => {
         })
 
 
-    const handleGetSongs = async (name) => {
+    const handleGetSongs = async (name, index) => {
         setSinger(name);
+        if(index !== selectedSinger) {
+            setSelectedSinger(index);
+            setSelectedSong(-1);
+        }
         const data = await fetch(`/songs/${name}`).then(res => res.json());
         console.log(data);
         setSongs(data.songs);
         // .then(res => res.json())
     }
 
-    const handleGetImage = async (song) => {
+    const handleGetImage = async (song, index) => {
+        setSelectedSong(index);
         const data = await fetch(`/songs/${singer}/${song}`).then(res => res.json());
         // const src = 'data:image/jpeg;base64,' + data.data.data.toString('base64');
         // SetImage(data.data);
         // SetImage(data.imageDir);
         // SetImage('file://Music/吉他谱/周杰伦/七里香1.png')
         console.log('dir', data);
-        SetImage(data.src);
+        SetImageSources(data.imageSources);
     }
 
+    // <div onClick={() => handleGetSongs(name)}>{name}</div>
+
     return (
-        <div className="Singers">
-            <div>
-                {
-                    names.map(name => <div onClick={() => handleGetSongs(name)}>{name}</div>)
-                }
+        <ThemeProvider theme={theme}>
+            <div className="Container">
+                    <div className="Singers_List">
+                        {
+                            names.map(
+                                (name, index) => 
+                                    <Button
+                                        variant={index === selectedSinger ? 'contained' : 'text'}
+                                        onClick={() => handleGetSongs(name, index)}
+                                    >
+                                        {name}
+                                    </Button>
+                            )
+                        }
+                    </div>
+                    <div className="Songs_List">
+                        {
+                            songs.map(
+                                (song, index) => 
+                                    <Button
+                                        variant={index === selectedSong ? 'contained' : 'text'}
+                                        color='song'
+                                        onClick={() => handleGetImage(song, index)}
+                                        sx={{
+                                            justifyContent: 'left',
+                                            whiteSpace: 'nowrap',
+                                            textOverflow: 'ellipsis',
+                                            maxWidth: '100%'
+                                        }}
+                                    >
+                                        {song}
+                                    </Button>
+                            )
+                        }
+                    </div>
+                
+                <div className="Image_List">
+                    {
+                        imageSources.map(
+                            src => 
+                                <img className="Image" src ={src}/>
+                        )
+                    }
+                </div>
             </div>
-            <div>
-                {
-                    songs.map(song => <div onClick={() =>  handleGetImage(song)}>{song}</div>)
-                }
-            </div>
-            <div>
-                {
-                    image && <img src ={image} width={800} height={1200}/>
-                }
-            </div>
-        </div>
+        </ThemeProvider>
     )
 }
 
