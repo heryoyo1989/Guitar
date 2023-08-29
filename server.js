d18449e7827ae1ec9f8117c97085ce5a11f3b153
Yue Yu <yueyu@YuedeMacBook-Pro.local>
2023-08-29 12:38:25 -0500

    add favlist, random pick, change singer

--- server.js
+++ server.js
@@ -77,6 +77,41 @@ app.listen(3000, function () {
   console.log('Example app listening on port 3000!\n');
 });
 
+app.get('/addFolder', async(req, res) => {
+    await client.connect();
+
+    const musicDB = client.db("Music");
+    const songsCollection = musicDB.collection("Songs");
+
+    const items = await songsCollection.find({ folder: 'null' }).toArray();
+    // console.log(items);
+
+    // await songsCollection.deleteMany({ folder: 'null'})
+
+    /* items.forEach(async item => {
+        const videoId = item.videoId;
+
+        await songsCollection.updateOne(
+            { song: item.song, folder: { $ne: 'null' } },
+            { $set: { videoId }}
+        )
+    }) */
+    
+    /* items.forEach(async item => {
+        if(item.folder[1] === '.') {
+            await songsCollection.updateOne(
+                { 
+                    song: item.song, 
+                    folder: item.folder
+                }, 
+                { $set: { singer: "" }}
+            )
+        }
+    }) */
+
+    // await songsCollection.updateMany({ singer: ''}, { $set: { singer: 'null' }});
+})
+
 app.get('/scan', async(req, res) => {
     await client.connect();
 
@@ -84,6 +119,8 @@ app.get('/scan', async(req, res) => {
     const singers = musicDB.collection("Singers");
     const songsCollection = musicDB.collection("Songs");
     const folderNames = fs.readdirSync(PARENT_DIR).filter(f => f !== '.DS_Store');
+
+    // TODO: Delete the node that is not in folderNames
 
     folderNames.forEach(async name => {
         const node = await singers.findOne({ name });
@@ -104,8 +141,6 @@ app.get('/scan', async(req, res) => {
         songNames.forEach(async song => {
             const existSong = await songsCollection.findOne({ song, singer: name })
 
-            console.log(name, song, existSong);
-
             if(!existSong) {
                 await songsCollection.insertOne({ song, singer: name, videoId: '' })
             }
@@ -122,22 +157,28 @@ app.get('/singers', async(req, res) => {
     })
 })
 
-app.get('/songs/:name', async(req, res) => {
-    const dir = `${PARENT_DIR}/${req.params.name}`;
-    const songs = fs.readdirSync(dir).filter(f => f !== '.DS_Store');
-    const songNames = getSongNames(songs);
+app.get('/songs/:folder', async(req, res) => {
+    // const dir = `${PARENT_DIR}/${req.params.folder}`;
+    // const files = fs.readdirSync(dir).filter(f => f !== '.DS_Store');
+    // const songNames = getSongNames(files);
 
-    res.json({ songs: songNames })
+    // TODO: Get favorite from DB
+    await client.connect();
+    const musicDB = client.db("Music");
+    const songs = await musicDB.collection("Songs").find({ folder: req.params.folder }).toArray();
+    // console.log("songs", songs);
+
+    res.json({ songs })
 })
 
-app.get('/songs/:name/:song', async(req, res) => {
-    const singerDir = `${PARENT_DIR}/${req.params.name}`;
+app.get('/songs/:folder/:song', async(req, res) => {
+    const singerDir = `${PARENT_DIR}/${req.params.folder}`;
     const songs = fs.readdirSync(singerDir);
     const images = songs.filter(song => song.indexOf(req.params.song) >= 0);
     const imageSources = [];
 
     images.forEach(image => {
-        const imageDir = `${PARENT_DIR}/${req.params.name}/${image}`;
+        const imageDir = `${PARENT_DIR}/${req.params.folder}/${image}`;
         const contents = fs.readFileSync(imageDir);
         const b64 = contents.toString('base64')
         const type = imageType(contents);
@@ -148,65 +189,45 @@ app.get('/songs/:name/:song', async(req, res) => {
     res.send({ imageSources })
 })
 
-/*
-app.get('/youtube1/:singer/:song', async(req, res) => {
-    const search = new SerpApi.GoogleSearch("0409427f527b9f517012a6556b6e7f2408639958d37602042d5cbc18432c50af");
-
-    const params = {
-        engine: "youtube",
-        search_query: `${req.params.singer === 'null'? '歌曲 ' : req.params.singer + ' '}${req.params.song}`,
-    };
-
-    const callback = function(data) {
-        console.log(data);
-        const videos = data.video_results;
-        
-        // inline_videos || data.organic_results.filter(video => video.link.indexOf('youtube') >= 0);
-
-        if(videos.length > 0) {
-            const link = videos[0].link;
-            const videoId = link.split('?')[1].split('=')[1];
-            const embedLink = `https://www.youtube.com/embed/${videoId}`;
-            res.send({ embedLink })
-        }
-        
-    };
-
-   search.json(params, callback);
-}) 
-*/
-
-app.get('/youtube/:singer/:song', async(req, res) => {
-    const singer = req.params.singer;
+app.get('/youtube/:folder/:song', async(req, res) => {
+    const folder = req.params.folder;
+    const singer = folder.indexOf('.') >= 0 ? 'null' : folder;
     const song = req.params.song;
 
     await client.connect();
     const musicDB = client.db("Music");
     const songs = musicDB.collection("Songs");
-    const songObject = await songs.findOne({ singer, song });
+    const songObject = await songs.findOne({ folder, song });
 
     if(songObject && songObject.videoId) {
         const videoId = songObject.videoId;
         const embedLink = `https://www.youtube.com/embed/${videoId}`;
-        res.send({ embedLink })
+        res.send({ embedLink, singer, videoId })
     } else {
-        const search_query =`${singer === 'null'? '歌曲 ' : singer + ' '}${song}`;
+        const search_query =`${songObject.singer === 'null'? '歌曲 ' : singer + ' '}${song}`;
 
         const response = await youtube.search.list({
             part: "snippet",
             q: search_query,
         });
 
-        console.log(response);
-        console.log(response.data.items);
+        // console.log(response);
+        // console.log(response.data.items);
 
         const videos = response.data.items;
             
         if(videos.length > 0) {
             const videoId = videos[0].id.videoId;
-            await songs.updateOne({ singer, song }, { $set: { videoId }});
+
+            if(songObject) {
+                await songs.updateOne({ singer, song, folder }, { $set: { videoId }});
+            } else {
+                await songs.insertOne({ singer, song, folder, videoId });
+
+                // TODO: insert to the singer list
+            }
             const embedLink = `https://www.youtube.com/embed/${videoId}`;
-            res.send({ embedLink })
+            res.send({ embedLink, singer, videoId })
         }
     }
 })
@@ -215,11 +236,92 @@ app.get('/mongo', async(req, res) => {
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
     const musicDB = client.db("Music");
     const singers = musicDB.collection("Singers");
     await singers.insertOne({
         name: '孙燕姿' + Math.random()
     });
+})
+
+app.get('/favourite/:folder/:song', async(req, res) => {
+    await client.connect();
+    const musicDB = client.db('Music');
+    const songs = musicDB.collection('Songs');
+
+    // console.log(req.params.folder + " : " + req.params.song);
+
+    await songs.updateOne(
+        { folder: req.params.folder, song: req.params.song },
+        { $set: { favourite: true }}
+    )
+})
+
+app.get('/unfavourite/:folder/:song', async(req, res) => {
+    await client.connect();
+    const musicDB = client.db('Music');
+    const songs = musicDB.collection('Songs');
+
+    // console.log("unfavourite", req.params.folder + " : " + req.params.song);
+
+    await songs.updateOne(
+        { folder: req.params.folder, song: req.params.song },
+        { $set: { favourite: false }}
+    )
+})
+
+app.get('/favourites', async(req, res) => {
+    await client.connect();
+    const musicDB = client.db('Music');
+    const songs = musicDB.collection('Songs');
+
+    const favourites = await songs.find({ favourite: true }).toArray();
+    // console.log("Favs", favourites);
+
+    res.json({ favourites })
+})
+
+app.get('/update/:folder/:song/:singer', async(req, res) => {
+    // update singer
+    await client.connect();
+    const musicDB = client.db('Music');
+    const songs = musicDB.collection('Songs');
+
+    const folder = req.params.folder;
+    const song = req.params.song;
+    const singer = req.params.singer;
+
+    console.log(req.params);
+    await songs.updateOne({ folder, song }, { $set: { singer }});
+    // get new videoId
+
+    const search_query =`${singer} ${song}`;
+
+    const response = await youtube.search.list({
+        part: "snippet",
+        q: search_query,
+    });
+
+    const videos = response.data.items;
+        
+    if(videos.length > 0) {
+        console.log(videos[0]);
+        const videoId = videos[0].id.videoId;
+        const embedLink = `https://www.youtube.com/embed/${videoId}`;
+
+        await songs.updateOne({ folder, song }, { $set: { videoId }});
+
+        res.json({ singer, videoId, embedLink })
+    }
+})
+
+app.get('/random', async(req, res) => {
+    await client.connect();
+    const musicDB = client.db('Music');
+    const songs = musicDB.collection('Songs');
+
+    const randomSongs = await songs.aggregate([{ $sample: { size: 1}}]).toArray();
+
+    res.json({ randomSongs })
 })
